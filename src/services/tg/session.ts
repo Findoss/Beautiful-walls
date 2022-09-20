2934ca954049c190a4c7b7e1e80ee68dcd906233
Nikita Stroganov <findoss@yandex.ru>
2022-09-21 00:28:09 +0300

    добавил сессии

--- src/services/tg/session.ts
+++ src/services/tg/session.ts
@@ -1,8 +1,18 @@
-// import type { MyContext } from './types';
 import type { Context } from 'grammy';
+import type { Report } from '../../models/report/types';
+
+import { debounce } from 'lodash';
+import { MyContext } from './types';
+import { sendReport } from '../../controllers/report';
 
 export const getSessionKey = (ctx: Context): string | undefined => {
-  return ctx.from === undefined || ctx.chat === undefined
-    ? undefined
-    : `${ctx.from.id}/${ctx.chat.id}`;
+  return ctx.chat?.id.toString();
 };
+
+export const createSession = (): Report => ({
+  imgs: [],
+  text: '',
+  check: debounce((ctx: MyContext) => {
+    sendReport(ctx);
+  }, 10000),
+});
